6fbd67ae92ddbae9cf1dc5e0c1173fb9c7532b10
Oreshcode <Oreshcode@gmail.com>
2024-06-26 19:51:43 +0300

    checkValidity newest

--- src/test/java/com/example/spring_into/service/impl/TollServiceImplTest.java
+++ src/test/java/com/example/spring_into/service/impl/TollServiceImplTest.java
@@ -16,9 +16,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import java.time.Instant;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
